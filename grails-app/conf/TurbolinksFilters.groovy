6eba14a3f3b73e6a1551225c25f8be8a45f4b3f8
Bobby Warner <bobbywarner@gmail.com>
2012-10-08 22:54:15 -0500

    tabs to spaces

--- grails-app/conf/TurbolinksFilters.groovy
+++ grails-app/conf/TurbolinksFilters.groovy
@@ -5,21 +5,21 @@
  * @author Bobby Warner
  */
 class TurbolinksFilters {
-	def filters = {
-		allURIs(uri: '/**') {
-			before = {
-				if (request.getHeader("X-XHR-Referer")) {
-            		log.info("X-XHR-Referer check true")
-            		
-        		} else {
-        			log.info("X-XHR-Referer check false")
-        			
-        		}
-			}
+    def filters = {
+        allURIs(uri: '/**') {
+            before = {
+                if (request.getHeader("X-XHR-Referer")) {
+                    log.info("X-XHR-Referer check true")
+                    
+                } else {
+                    log.info("X-XHR-Referer check false")
+                    
+                }
+            }
 
-			after = { Map model ->
-				response.addHeader('X-XHR-Current-Location', request.forwardURI)
-			}
-		}
-	}
+            after = { Map model ->
+                response.addHeader('X-XHR-Current-Location', request.forwardURI)
+            }
+        }
+    }
 }
