57932b5457a0deeb9bf52aabab7e7e0259c3e7cf
Bobby Warner <bobbywarner@gmail.com>
2014-01-12 15:58:17 -0600

    Cleaned up BuildConfig

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,9 +1,7 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
+grails.project.work.dir = 'target'
 
 grails.project.dependency.resolution = {
-    inherits("global") {}
+    inherits "global"
     log "warn"
 
     repositories {
@@ -15,7 +13,7 @@ grails.project.dependency.resolution = {
     dependencies {}
 
     plugins {
-        compile(":asset-pipeline:1.2.4",":coffee-asset-pipeline:1.2.0") {
+        compile(":asset-pipeline:1.2.4", ":coffee-asset-pipeline:1.2.0") {
             export = false
         }
 
